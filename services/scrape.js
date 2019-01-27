8117f6f8ad5e1e9373ff0387d2f5c4d4f8ef576d
geon yoon <geonyoon@1x-nat-vl931-172-30-129-191.wireless.umass.edu>
2019-01-27 18:07:52 -0500

    Replace request to axios due to async funciton

--- services/scrape.js
+++ services/scrape.js
@@ -1,43 +1,32 @@
-const request = require('request');
 const cheerio = require('cheerio');
-
-module.exports = app => {
-    request('https://www.sciencedaily.com/news/computers_math/computer_programming/',(error,
-        response,html) => {
-            if(!error & response.statusCode ==200){
-                const $ = cheerio.load(html);
-                // const article = $('.list-group')
-                const article = $('.list-group a')
-                                .each((index,element) => {
-                                    const item = $(element).text()
-                                    const link = $(element).attr('href')
-                                    const sublink = 'https://www.sciencedaily.com'+link
-
-                                    request(sublink, (error,response,html) => {
-                                        if(!error & response.statusCode == 200){
-                                            const $2 = cheerio.load(html)
-                                            // const sub_article = $2('.dl-horizontal').children().next().next().next()
-                                            
-                                            const headline = $2('.headline').text()
-                                            console.log(headline)
-                                            const sub_article = $2('.dl-horizontal')
-                                                                    .each((index, element) => {
-                                                                        const item = $2(element).find('#source')
-                                                                        // console.log(item.html())
-                                                                    })
-                                            // console.log(sub_article.html())
-                                        }   
-                                        else{
-                                            console.log("sublink has an error")
-                                        }
-                                    })
-                                });
-                                
-
-                // console.log(article.html())
-            }   
-            else{
-                console.log("link has an error")
-            }
-    });
+const axios = require("axios");
+ 
+module.exports = async function test(app){
+    var sub_link = [] 
+    var data = []
+    const main_url = await axios.get('https://www.sciencedaily.com/news/computers_math/computer_programming/')
+    var $ = cheerio.load(main_url.data);
+    
+    // Get the sub links and add them into the list
+    $('.list-group a').each((index,element) => {
+        const link = $(element).attr('href')
+        sub_link.push('https://www.sciencedaily.com'+link);
+    })
+    
+    for (var i = 0; i<sub_link.length; i++){
+        var inner_data = []
+        inner_data.push(sub_link[i])
+        const sub_url = await axios.get(sub_link[i])
+        var $ = cheerio.load(sub_url.data);
+        const title = $('.headline').text()
+        inner_data.push(title);
+        $('.dl-horizontal').each((index, element) => {
+            const date = $(element).find('#date_posted').text()
+            const source = $(element).find('#source').text()
+            const summary = $(element).find('#abstract').text() 
+            inner_data.push(date,source,summary)   
+        })
+        data.push(inner_data);
+    }
+    console.log(data);
 }
